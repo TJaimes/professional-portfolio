dbc07881cec9285d3c02fbaf935f4486dfacc525
Tairi Raquel Jaimes Salazar <tjaimes@centralinformatica.com>
2023-12-27 15:24:11 -0600

    Nuevo: módulo banner

---
+++ src/components/Banner.js
@@ -0,0 +1,61 @@
+import {useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap"
+import meHeader from "../assets/img/meBanner.png";
+import headerPrueba from "../assets/img/imgPrueba.png";
+
+export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const [index, setIndex] = useState(1);
+    const toRotate = [ "Web Developer", "Web Full-Stack", "Back-End Developer" ];
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta);
+
+        return () => { clearInterval(ticker) };
+        }, [text])
+
+        const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2);
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setIndex(prevIndex => prevIndex - 1);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setIndex(1);
+            setDelta(500);
+        } else {
+            setIndex(prevIndex => prevIndex + 1);
+        }
+    }
+
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="aligh-items-center">
+                    <div>
+                        <img src={headerPrueba} alt="Header Img"/>
+                        <h1>Hi! I'm Tairi Jaimes</h1>
+                        <h1><span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                        </div>
+                </Row>
+            </Container>
+        </section>
+    )
+}
