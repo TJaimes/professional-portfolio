220b77438b08835ca1bf1306bdd702d92ddba076
Tairi Raquel Jaimes Salazar <tjaimes@centralinformatica.com>
2024-01-10 12:18:05 -0600

    Modificacion: agregar links a los Ãsocial icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,16 +7,20 @@ import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
-        const handleDownload = (e) =>{
-            e.preventDefault();
+    const handleDownload = (e) =>{
+        e.preventDefault();
 
-            const link = document.createElement('a');
-            link.href = process.env.PUBLIC_URL + '/CV-TairiJaimes.pdf';
-            link.download = 'CV-TairiJaimes.pdf';
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        };
+        const link = document.createElement('a');
+        link.href = process.env.PUBLIC_URL + '/CV-TairiJaimes.pdf';
+        link.download = 'CV-TairiJaimes.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
+    const handleLinkedInClick = () => {
+        window.location.href = 'https://www.linkedin.com/in/dev-tairi-jaimes';
+    };
 
     return (
         <footer className="footer" id="footer">
@@ -42,9 +46,15 @@ export const Footer = () => {
                             <div className="foot-icon">
                                 <a href="#" onClick={handleDownload}><FileEarmarkArrowDownFill/>CV</a>
                             </div>
-                            <a href=""><img src={navIcon1}/></a>
-                            <a href=""><img src={navIcon2}/></a>
-                            <a href=""><img src={navIcon3}/></a>
+                            <a href="https://www.linkedin.com/in/dev-tairi-jaimes" target="_blank" rel="noopener noreferrer">
+                                <img src={navIcon1}/>
+                            </a>
+                            <a href="https://www.facebook.com/tairiraquel.jaimessalazar" target="_blank" rel="noopener noreferrer">
+                                <img src={navIcon2}/>
+                            </a>
+                            <a href="https://www.instagram.com/z_jms/" target="_blank" rel="noopener noreferrer">
+                                <img src={navIcon3}/>
+                            </a>
                         </div>
                     </Col>
             </Row>
